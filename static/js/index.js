005b9745b413daf7fb94cdfa2684ee3df47b7f65
Mainak Mondal <pdasp-t@uconn.edu>
2026-03-11 00:43:08 -0400

    change authorship for domain

---
+++ static/js/index.js
@@ -0,0 +1,13 @@
+﻿function scrollToTop() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+window.addEventListener('scroll', function () {
+  const btn = document.querySelector('.scroll-to-top');
+  if (!btn) return;
+  if (window.scrollY > 300) {
+    btn.classList.add('visible');
+  } else {
+    btn.classList.remove('visible');
+  }
+});
